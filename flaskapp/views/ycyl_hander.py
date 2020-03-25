ca60c945545e94c9dbfc4ec25a695778c233065f
LUCY <biyan_111@126.com>
2020-03-25 14:08:17 +0800

    Add ycyl ajax form data post module.

--- flaskapp/views/ycyl_hander.py
+++ flaskapp/views/ycyl_hander.py
@@ -5,17 +5,24 @@
 # @File    : ycyl_hander.py
 # @Software: PyCharm
 from flask import Blueprint, flash, render_template
+from ..common import ConsoleLogger, relative_path
+from flask import request
+import json
 
-
+logger = ConsoleLogger(relative_path(__file__))
 ycyl = Blueprint('ycyl', __name__, url_prefix='/ycyl')
 
 
 @ycyl.route('/', methods=["GET", "POST"])
 def home():
-    """
-    使用flash来进行消息提示推送
-    页面使用{{ get_flashed_messages()[0] }} 获取
-    """
-    #flash("welcome !")
-
     return render_template("ycyl.html")
+
+
+@ycyl.route('/api/register', methods=["POST"])
+def register():
+    data = json.loads(request.form.get('data'))
+    logger.info(data)
+    logger.info(data['x'])
+
+    return render_template("开通成功！")
+
